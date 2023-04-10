e8a4ddda0ac97c0ece1128d104ef04af52b5294b
Obstinate <mathis.beauville@gmail.com>
2023-04-10 13:28:02 +0200

    init

---
+++ colors/vesperpp.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local vesperpp = require("vesperpp")
+vesperpp.setup({})
+EOF
---
+++ lua/vesperpp/palette.lua
@@ -0,0 +1,22 @@
+local colors = {
+  fg = "#FFFFFF",
+  bg = "#101010",
+  gray = "#595959",
+  light_gray = "#cccccc",
+  line = "#505050",
+  red = "#FF8080",
+  blue = "#99FFE4",
+  orange = "#FFC799",
+  magenta = "#FBADFF",
+  cursor_fg = "#515052",
+  cursor_bg = "#AEAFAD",
+  error_bg = "#31262d",
+  warn_bg = "#32302f",
+  info_bg = "#1e3135",
+  hint_bg = "#22323f",
+  reference = "#363636",
+  success_green = "#14C50B",
+  folder_blue = "#42A5F5",
+}
+
+return colors
